12d94f02f00609d886661b8cd8274a72b5bdebf1
techkodeninja <164923145+techkodeninja@users.noreply.github.com>
2025-05-14 19:02:04 -0700

    update

--- src/replace.mjs
+++ src/replace.mjs
@@ -27,64 +27,61 @@ const doReplacePhp = async ( conf, ignoreFile ) => {
 			".git/**/*",
 			ignoreFile
 		],
-
 		files : [
 			`${themeRoot}/**/*.php`,
 			`${themeRoot}/*.js`,
 			`${themeRoot}/webpack.*.js`,
 			`${themeRoot}/public/views/**/*.php`
 		],
-
 		from : [
 			// ✅ Namespace Replacement
 			new RegExp( `namespace ${casex( conf.from.Name, 'CaSe' )}`, "g" ),
-
+			
 			// ✅ Package Replacement (align with other tags)
 			new RegExp( `(@package\\s+)(\\s*)${casex( conf.from.Name, 'CaSe' )}`, "g" ),
-
+			
 			// ✅ Variable Replacement
 			new RegExp( `\\$${casex( conf.from.Name.toLowerCase(), 'ca_se' )}`, "g" ),
-
+			
 			// ✅ Path Replacement
 			new RegExp( `${casex( conf.from.Name.toLowerCase(), 'ca_se' )}/`, "g" ),
-
+			
 			// ✅ URI Replacement
 			new RegExp( conf.from.Uri, "g" ),
 			new RegExp( conf.from.AuthorUri, "g" ),
-
+			
 			// ✅ Author Information
 			new RegExp( conf.from.AuthorEmail, "g" ),
 			new RegExp( conf.from.Author, "g" ),
-
+			
 			// ✅ Copyright Replacement (Match any year)
 			new RegExp( `@copyright\\s+([0-9]{4})\\s+${conf.from.Author}`, "g" ),
 			new RegExp( `©\\s+([0-9]{4})\\s+${conf.from.Author}`, "g" ),
-
+			
 			// ✅ If there is no year, just the author
 			new RegExp( `©\\s+${conf.from.Author}`, "g" )
 		],
-
 		to : [
 			// ✅ Namespace Replacement
 			`namespace ${casex( conf.to.Name, 'CaSe' )}`,
-
+			
 			// ✅ Package Replacement with alignment
 			`${padRight( "@package", maxTagLength )} ${casex( conf.to.Name, 'CaSe' )}`,
-
+			
 			// ✅ Variable Replacement
 			`\$${casex( conf.to.Name.toLowerCase(), 'ca_se' )}`,
-
+			
 			// ✅ Path Replacement
-			`${casex( conf.to.Name.toLowerCase(), 'ca_se')}/`,
-
+			`${casex( conf.to.Name.toLowerCase(), 'ca_se' )}/`,
+			
 			// ✅ URI Replacement
 			conf.to.Uri,
 			conf.to.AuthorUri,
-
+			
 			// ✅ Author Information
 			conf.to.AuthorEmail,
 			conf.to.Author,
-
+			
 			// ✅ Copyright Replacement
 			`${padRight( "@copyright", maxTagLength )} ${conf.to.Year} ${conf.from.Author}`,
 			`© ${conf.to.Year} ${conf.from.Author}`,
@@ -96,11 +93,9 @@ const doReplacePhp = async ( conf, ignoreFile ) => {
 const doReplaceStyleCss = async ( conf ) => {
 	return {
 		allowEmptyPaths : true,
-
-		files : [
+		files           : [
 			`${themeRoot}/style.css`
 		],
-
 		from : [
 			new RegExp( `(Theme Name:\\s+).*`, "g" ),
 			new RegExp( `(Theme URI:\\s+).*`, "g" ),
@@ -109,14 +104,13 @@ const doReplaceStyleCss = async ( conf ) => {
 			new RegExp( `(Description:\\s+).*`, "g" ),
 			new RegExp( `(Text Domain:\\s+).*`, "g" )
 		],
-
 		to : [
-			`Theme Name:     ${casex( conf.to.Name, 'CaSe' )}`,
-			`Theme URI:      ${conf.to.Uri}`,
-			`Author:         ${conf.to.Author}`,
-			`Author URI:     ${conf.to.AuthorUri}`,
-			`Description:    ${conf.to.Description}`,
-			`Text Domain:    ${casex( conf.to.Name, 'ca-se' ).toLowerCase()}`
+			`Theme Name:   ${casex( conf.to.Name, 'CaSe' )}`,
+			`Theme URI:    ${conf.to.Uri}`,
+			`Author:       ${conf.to.Author}`,
+			`Author URI:   ${conf.to.AuthorUri}`,
+			`Description:  ${conf.to.Description}`,
+			`Text Domain:  ${casex( conf.to.Name, 'ca-se' ).toLowerCase()}`
 		]
 	};
 };
